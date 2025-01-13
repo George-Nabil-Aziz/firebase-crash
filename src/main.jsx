cef6ed5f8f09b5ee4736c840f7f01703c90daa2f
George <george.nabil.biy@gmail.com>
2025-01-13 22:02:51 +0200

    Adding delete user page

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import "./index.css";
 import { Login } from "./components/_01Login.jsx";
 import { Signup } from "./components/_02Signup.jsx";
 import { AuthComponent } from "./components/_03Auth.jsx";
-import { SaveNumber } from "./components/_04SaveNumber.jsx";
-import { RestoreNumber } from "./components/_05RestoreNumber.jsx";
-import { DeleteNumber } from "./components/_06DeleteNumber.jsx";
-import { RestoreSpecificId } from "./components/_07RestoreSpecificId.jsx";
+import { SaveNumber } from "./components/_11SaveNumber.jsx";
+import { RestoreNumber } from "./components/_12RestoreNumber.jsx";
+import { DeleteNumber } from "./components/_13DeleteNumber.jsx";
+import { RestoreSpecificId } from "./components/_14RestoreSpecificId.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
