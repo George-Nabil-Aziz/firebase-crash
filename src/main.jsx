96472c4dfe9a2128257a336860d59aabeb06fccf
George <george.nabil.biy@gmail.com>
2025-01-13 22:48:40 +0200

    Main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+// Authentication
 import { Login } from "./components/_01Login.jsx";
 import { Signup } from "./components/_02Signup.jsx";
 import { AuthComponent } from "./components/_03Auth.jsx";
+import { DeleteUser } from "./components/_04Delete.jsx";
+
+// Methods
 import { SaveNumber } from "./components/_11SaveNumber.jsx";
 import { RestoreNumber } from "./components/_12RestoreNumber.jsx";
 import { DeleteNumber } from "./components/_13DeleteNumber.jsx";
@@ -18,6 +22,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <hr />
     <AuthComponent />
     <hr />
+    <DeleteUser />
+    <hr />
+    <hr />
+
     <SaveNumber value={2} />
     <hr />
     <RestoreNumber />
